84488ea47bb63f0356ae7875c0b66ac11ffcf615
onracr <onracr_ms87@hotmail.com>
2019-12-11 14:42:09 +0300

    Commit 0.0.7
    Implemented Raycast to a navmesh and adjusted navmeshpath that player can move through various distances.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,18 +1,20 @@
 using System;
 using Combat;
-using Core;
 using Movement;
 using Resources;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 namespace Control
 {
     public class PlayerController : MonoBehaviour 
     {
-        // References
+        // References, Config variables
         [SerializeField] private new Camera camera = null;
         [SerializeField] private CursorMapping[] cursorMappings = null;
+        [SerializeField] private float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] private float maxNavPathLength = 40f;
         
         [System.Serializable]
         struct CursorMapping
@@ -94,14 +96,64 @@ namespace Control
         }
         private bool MovementInteraction()
         {
-            if (Physics.Raycast(GetMouseRay(), out var hit)) {
-                if (Input.GetMouseButton(0)) {
-                    _mover.StartMoveAction(hit.point, 1f);
+            //if (Physics.Raycast(GetMouseRay(), out var hit)) {
+            Vector3 target;
+            bool hasHit = RaycastNavMesh(out target);
+            if (hasHit)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    _mover.StartMoveAction(target, 1f);
                 }
                 SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
+        }
+
+        private bool RaycastNavMesh(out Vector3 target)
+        {
+            target = new Vector3();
+
+            RaycastHit hit;
+            var hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (!hasHit) return false;
+
+            NavMeshHit navMeshHit;
+            bool hasCastToNavMesh = NavMesh.SamplePosition(
+                hit.point,
+                out navMeshHit,
+                maxNavMeshProjectionDistance,
+                NavMesh.AllAreas
+                );
+            if (!hasCastToNavMesh) return false;
+
+            target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(
+                transform.position,
+                target,
+                NavMesh.AllAreas,
+                path
+                );
+
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > maxNavPathLength) return false;
+
+            return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0f;
+            if (path.corners.Length < 2) return total;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+            return total;
         }
 
         private void SetCursor(CursorType type)
